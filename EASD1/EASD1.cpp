0bd4c530fea98096b2d5672aa12fd1d3a5b9c537
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-07 09:30:17 +0700

    sebagian jawaban no 3

--- EASD1/EASD1.cpp
+++ EASD1/EASD1.cpp
@@ -1,3 +1,5 @@
 // 1. Karena algoritma menyediakan langkah langkah terstruktur dan terurut secara logis untuk menyelesaikan masalah pada pengcodingan
 
 // 2. Struktur data linear dan struktur data non linear
+
+// 3. Waktu eksekusi, besar memori yang digunakan, kualitas rumus, 
