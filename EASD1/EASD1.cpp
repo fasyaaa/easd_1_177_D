e0f7a61a120e0b9e6e05c9e25a55235c8bac7154
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-07 09:24:27 +0700

    jawaban no 2

--- EASD1/EASD1.cpp
+++ EASD1/EASD1.cpp
@@ -1,2 +1,3 @@
 // 1. Karena algoritma menyediakan langkah langkah terstruktur dan terurut secara logis untuk menyelesaikan masalah pada pengcodingan
 
+// 2. Struktur data linear dan struktur data non linear
