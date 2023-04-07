19a7441734d4910a9a19b198de8df8e6b2f46f3d
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-07 09:48:47 +0700

    no 5

--- EASD1/EASD1.cpp
+++ EASD1/EASD1.cpp
@@ -1,10 +1,12 @@
-// 1. Karena algoritma menyediakan langkah langkah terstruktur dan terurut secara logis untuk menyelesaikan masalah pada pengcodingan
+// 1.	Karena algoritma menyediakan langkah langkah terstruktur dan terurut secara logis untuk menyelesaikan masalah pada pengcodingan
 
 // 2.	a. Struktur data linear
 //		b. Struktur data non linear
 
-// 3. Waktu eksekusi, besar memori yang digunakan, banyaknya fungsi yang dipakai
+// 3.	Waktu eksekusi, besar memori yang digunakan, banyaknya fungsi yang dipakai
 
-// 4. insertion sort, dikarenakan algoritma ini bekerja dengan cara membandingkan beberapa elemen
+// 4.	insertion sort, dikarenakan algoritma ini bekerja dengan cara membandingkan beberapa elemen
 //		dalam daftar kemudian dimasukkan ke posisi yang tepat dalam daftar secara urut
 
+// 5.	a. Quadratic = selection sort, insertion sort, bubble sort
+//		b. Loglinear = merge sort dan quick sort
